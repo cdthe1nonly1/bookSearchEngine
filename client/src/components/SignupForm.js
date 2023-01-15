354650a8f09801a9fc02a4137684527fcb2f95e3
Charles Dykstra <charles.b.dykstra@gmail.com>
2023-01-15 15:41:01 -0700

    updated the components, changed const for the mutations and added variables

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+
+import { CREATE_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { useMutation } from "@apollo/client"; 
 
 const SignupForm = () => {
   // set initial form state
@@ -28,15 +30,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await createUser({variables: {...userFormData}});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      
+     Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
